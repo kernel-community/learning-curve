ef9a3dcae2711b7144abb18ae66f8380544b9aab
kjr217 <kjraja217@gmail.com>
2021-06-17 01:09:14 +0100

    feat: learning curve contracts, kernel contracts, full flow tests for yield and non-yield workflows. Note: Full unittests are WIP.

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # The Kernel Learning Curve
 
 Smart contracts for free and continuous online learning environments, which nevertheless ensure that course designers are properly rewarded for their work.
+
+## Testing and Development
+Note: Test suite is WIP, the current tests only do a full flow success test and does not currently go for full coverage or run scenarios.
+
+This repository uses brownie for testing.
+### Dependencies
+
+* [python3](https://www.python.org/downloads/release/python-368/) version 3.6 or greater, python3-dev
+* [brownie](https://github.com/iamdefinitelyahuman/brownie) - tested with version [1.14.6](https://github.com/eth-brownie/brownie/releases/tag/v1.14.6)
+* [ganache-cli](https://github.com/trufflesuite/ganache-cli) - tested with version [6.12.2](https://github.com/trufflesuite/ganache-cli/releases/tag/v6.12.2)
+
+To run the non-mainnet test suite (no yield functionality)
+
+```
+cd learning-curve
+brownie test tests -s
+```
+
+To run the mainnet test suite (yield functionality):
+1. Add a WEB3_INFURA_PROJECT_ID as an [environmental variable](https://eth-brownie.readthedocs.io/en/stable/network-management.html#using-infura)
+2. Add an ETHERSCAN_TOKEN as an environmental variable
+3. Run the following
+```
+cd learning-curve
+brownie test tests-mainnet --network=mainnet-fork -s
+```
+
+## Current gas report
+```
+KernelFactory <Contract>
+   ├─ constructor  -  avg: 1231891  avg (confirmed): 1231891  low: 1231891  high: 1231891
+   ├─ mint         -  avg:  102775  avg (confirmed):  102775  low:  102225  high:  103353
+   ├─ redeem       -  avg:   87439  avg (confirmed):   87439  low:   87112  high:   87989
+   ├─ batchDeposit -  avg:   65530  avg (confirmed):   65530  low:   65530  high:   65530
+   ├─ createCourse -  avg:   45858  avg (confirmed):   45858  low:   45858  high:   45858
+   └─ register     -  avg:   38673  avg (confirmed):   38673  low:   38673  high:   38673
+LearningCurve <Contract>
+   ├─ constructor  -  avg: 1321662  avg (confirmed): 1321662  low: 1321662  high: 1321662
+   ├─ burn         -  avg:   70047  avg (confirmed):   70047  low:   69838  high:   70198
+   ├─ initialise   -  avg:   45179  avg (confirmed):   45179  low:   45179  high:   45179
+   └─ approve      -  avg:   29295  avg (confirmed):   29295  low:   29285  high:   29309
+```
--- contracts/LearningCurve.sol
+++ contracts/LearningCurve.sol
@@ -1,70 +1,115 @@
-"SPDX-License-Identifier: MPL-2.0"
+//SPDX-License-Identifier: MPL-2.0
+pragma solidity 0.8.0;
 
-pragma solidity 0.8.3;
+import "./PRBMath.sol";
+import "./PRBMathUD60x18.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
- * @title LearningCurve
- * @dev A simple constant product curve that mints LEARN tokens whenever
- * anyone sends it DAI, or burns LEARN tokens and returns DAI.
+ * @title  LearningCurve
+ * @author kjr217
+ * @notice A simple constant product curve that mints LEARN tokens whenever
+ *         anyone sends it DAI, or burns LEARN tokens and returns DAI.
  */
-
-import "@open-zeppelin/contracts/token/ERC20/ERC20.sol";
-
-/**
- * Consider adding this trick to prevent transfers without socializing gas costs
- * https://github.com/BellwoodStudios/optimism-dai-bridge/blob/master/contracts/l2/dai.sol#L76-L77
- * from https://twitter.com/godsflaw/status/1379101053766017029
- */
-
 contract LearningCurve is ERC20 {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
-    /**@dev the constant product used in the curve */
-    uint product public constant = 10000; 
+    // the constant product used in the curve
+    uint public constant k = 10000;
+    IERC20 public reserve;
+    uint256 public reserveBalance;
+    bool initialised;
 
-    /**
-    * @dev this method allows anyone to mint LEARN tokens dependent on the
-    * amount of DAI they send. 
-    *
-    * The amount minted depends on the amount of collateral already locked in
-    * the curve. The more DAI is locked, the less LEARN gets minted, ensuring 
-    * that the price of LEARN increases linearly.
-    *
-    * Please see: https://docs.google.com/spreadsheets/d/1hjWFGPC_B9D7b6iI00DTVVLrqRFv3G5zFNiCBS7y_V8/edit?usp=sharing
-    */
-    function mint() public {
-
+    constructor (address _reserve) ERC20("Learning Curve", "LEARN"){
+        reserve = IERC20(_reserve);
     }
 
     /**
-    * @dev same story here, except that an address is passed in which the minted 
-    * LEARN is sent to. Necessary to allow for mints directly from a Course, where
-    * we want to learner to receive LEARN, not the course contract.
-    *
-    * Can be used to send DAI from one address and have LEARN returned to another.
-    * Is this a problem? I think not...
+    * @notice initialise the contract, mainly for maths purposes, requires the transfer of 1 DAI.
+    * @dev    only callable once
     */
-    function mint(address learner) public {
-
+    function initialise() external {
+        require(!initialised, "initialised");
+        initialised = true;
+        reserve.safeTransferFrom(msg.sender, address(this), 1e18);
+        reserveBalance += 1e18;
+        _mint(address(this), 10001e18);
     }
 
     /**
-    * @dev used to burn LEARN and return DAI to the sender. Uses the same simple
-    * curve to calculate the correct amounts.
+    * @notice      This method allows anyone to mint LEARN tokens dependent on the
+    *              amount of DAI they send.
+    *
+    *              The amount minted depends on the amount of collateral already locked in
+    *              the curve. The more DAI is locked, the less LEARN gets minted, ensuring
+    *              that the price of LEARN increases linearly.
+    *
+    *              Please see: https://docs.google.com/spreadsheets/d/1hjWFGPC_B9D7b6iI00DTVVLrqRFv3G5zFNiCBS7y_V8/edit?usp=sharing
+    * @param  _wad amount of Dai to send to the contract
     */
-    function burn() public {
-
+    function mint(uint256 _wad) public {
+        require(initialised, "!initialised");
+        reserve.safeTransferFrom(msg.sender, address(this), _wad);
+        uint256 ln = doLn((reserveBalance.add(_wad).mul(1e18)).div(reserveBalance)) ;
+        uint256 learnMagic = k.mul(ln);
+        reserveBalance += _wad;
+        _mint(msg.sender, learnMagic);
     }
 
     /**
-    * @dev calculates the amount of LEARN to mint or burn given the amount of DAI
-    * or LEARN sent to the curve contract.
+    * @notice        Same as normal mint, except that an address is passed in which the minted
+    *                LEARN is sent to. Necessary to allow for mints directly from a Course, where
+    *                we want to learner to receive LEARN, not the course contract.
     *
-    * Not internal because anyone should be able to call it to get the price of 
-    * LEARN according to the contract. Could be split into 2 different functions
-    * though, one internal, one which just returns price for convenience on any FE.
+    *                Can be used to send DAI from one address and have LEARN returned to another.
+    * @param learner address of the learner to mint LEARN to
+    * @param _wad    amount of DAI being sent in.
     */
-    function calcPrice(uint256 received) view returns(uint256 price) {
-        return uint256 price = received / product;
+    function mintForAddress(address learner, uint256 _wad) public {
+        require(initialised, "!initialised");
+        reserve.safeTransferFrom(msg.sender, address(this), _wad);
+        uint256 ln = doLn((reserveBalance.add(_wad).mul(1e18)).div(reserveBalance)) ;
+        uint256 learnMagic = k.mul(ln);
+        reserveBalance += _wad;
+        _mint(learner, learnMagic);
     }
-    
+
+    /**
+    * @notice               used to burn LEARN and return DAI to the sender. The amount of dai that the burner wants
+    *                       to be received should be sent in, this is because providing a Learn and converting to DAI
+    *                       is complex mathematically.
+    * @param  _daiToReceive amount of dai the burner would like to receive
+    */
+    function burn(uint256 _daiToReceive) public {
+        require(initialised, "!initialised");
+        uint256 ln = doLn((reserveBalance.mul(1e18)).div(reserveBalance.sub(_daiToReceive)));
+        uint256 learnMagic = k.mul(ln);
+        _burn(msg.sender, learnMagic);
+        reserveBalance -= _daiToReceive;
+        reserve.safeTransfer(msg.sender, _daiToReceive);
+    }
+
+    /**
+    * @notice   Calculates the natural logarithm of x.
+    * @param  x the number to be magic'd
+    */
+    function doLn(uint256 x) internal pure returns (uint256 result) {
+        PRBMath.UD60x18 memory xud = PRBMath.UD60x18({ value: x });
+        result = PRBMathUD60x18.ln(xud).value;
+    }
+
+    /**
+    * @notice               calculates the amount of LEARN to burn given the amount of DAI requested.
+    * @param  reserveAmount the amount of DAI to burn
+    */
+    function getBurnableForReserveAmount(uint256 reserveAmount) external view returns (uint256 learnMagic){
+        uint256 ln = doLn((reserveBalance.mul(1e18)).div(reserveBalance.sub(reserveAmount)));
+        learnMagic = k.mul(ln);
+    }
+
 }
