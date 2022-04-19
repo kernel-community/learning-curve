53b890eb2453438fe46e4e3566a3d02d03508a06
andytudhope <tuddy0525@gmail.com>
2022-04-19 20:54:17 +0200

    One iteration at a time

--- contracts/KernelFactory.sol
+++ contracts/KernelFactory.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MPL-2.0
-pragma solidity 0.8.13;
+pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
